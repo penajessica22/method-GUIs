32f124683c6d24cd0f423ca6274bd0a6105ee2f7
Jessica Pena <penajessica123@yahoo.com>
2019-05-01 19:13:01 -0500

    created gui assignment

---
+++ main.js
@@ -0,0 +1,12 @@
+
+// function test(){
+
+// console.log("hello")
+// }
+function addition(num1, num2){
+    let num1 = 2
+    let num2 = 3
+    let total = num1 + num2;
+    return total;
+    console.log('total')
+   }
